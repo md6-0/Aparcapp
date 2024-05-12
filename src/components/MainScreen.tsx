4cd6ce62191c6b605cf4dc1895d7448c63c426c3
Mario Díaz <32886429+md6-0@users.noreply.github.com>
2024-05-12 12:15:33 +0200

    Limpieza de paquetes

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import MainLocationCard from './MainLocationCard';
 import { Geolocation } from '@capacitor/geolocation';
 import { IonButton, IonSpinner } from '@ionic/react';
