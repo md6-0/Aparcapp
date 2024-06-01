0b85ffc736856eee3997cbe44dddfc50d162caa3
Mario Díaz <32886429+md6-0@users.noreply.github.com>
2024-06-01 15:40:08 +0200

    Cambio nombre package

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.tfwstudios.aparcapp',
   appName: 'Aparcapp',
   webDir: 'dist'
 };
